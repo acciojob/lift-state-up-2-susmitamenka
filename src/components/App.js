28f6edda218d1b0abd773e645c81cae58b922f42
susmitamenka <susmitaprasad73@gmail.com>
2025-05-09 19:50:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+"
+import Parent from "./parent";
 
 const App = () => {
   return (
     <div>
+   <Parent/>
         {/* Do not remove the main div */}
     </div>
   )
