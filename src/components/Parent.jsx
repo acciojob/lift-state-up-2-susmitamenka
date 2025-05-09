94abec225e9eb610481532b25c8cc9a039e86d35
susmitamenka <susmitaprasad73@gmail.com>
2025-05-09 19:51:39 +0530

    Update Parent.jsx

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,1 +1,17 @@
 
+
+import React from "react";
+import './../styles/App.css';
+import Parent from "./parent";
+
+const App = () => {
+  return (
+    <div>
+        {/* Do not remove the main div */}
+        <Parent/>
+    </div>
+  )
+}
+
+export default App
+
